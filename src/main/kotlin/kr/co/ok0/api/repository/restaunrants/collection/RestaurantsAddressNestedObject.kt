4aebd1c2317c3fe81b1f1076020ced859eb3aa2c
ok0 <ongnya@gmail.com>
2022-12-22 22:00:23 +0900

    Add basic methods.

--- src/main/kotlin/kr/co/ok0/api/repository/restaunrants/collection/RestaurantsAddressNestedObject.kt
+++ src/main/kotlin/kr/co/ok0/api/repository/restaunrants/collection/RestaurantsAddressNestedObject.kt
@@ -14,4 +14,30 @@ class RestaurantsAddressNestedObject(
 
   @Field(name = "zipcode")
   var zipCode: String
-)
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as RestaurantsAddressNestedObject
+
+    if (building != other.building) return false
+    if (coord != other.coord) return false
+    if (street != other.street) return false
+    if (zipCode != other.zipCode) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = building.hashCode()
+    result = 31 * result + (coord?.hashCode() ?: 0)
+    result = 31 * result + street.hashCode()
+    result = 31 * result + zipCode.hashCode()
+    return result
+  }
+
+  override fun toString(): String {
+    return "RestaurantsAddressNestedObject(building='$building', coord=$coord, street='$street', zipCode='$zipCode')"
+  }
+}
--- src/main/kotlin/kr/co/ok0/api/repository/restaunrants/collection/RestaurantsGradesNestedObject.kt
+++ src/main/kotlin/kr/co/ok0/api/repository/restaunrants/collection/RestaurantsGradesNestedObject.kt
@@ -11,8 +11,35 @@ class RestaurantsGradesNestedObject(
   @Field(name = "grade")
   var grade: String,
   // TODO: 2022/12/22 use enum class
-//  var gradeTest: RestaurantsGradeType,
+  @Field(name = "grade")
+  var gradeTest: RestaurantsGradeType,
 
   @Field(name = "score")
   var score: Int?
-)
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as RestaurantsGradesNestedObject
+
+    if (date != other.date) return false
+    if (grade != other.grade) return false
+    if (gradeTest != other.gradeTest) return false
+    if (score != other.score) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = date.hashCode()
+    result = 31 * result + grade.hashCode()
+    result = 31 * result + gradeTest.hashCode()
+    result = 31 * result + (score ?: 0)
+    return result
+  }
+
+  override fun toString(): String {
+    return "RestaurantsGradesNestedObject(date=$date, grade='$grade', gradeTest=$gradeTest, score=$score)"
+  }
+}
